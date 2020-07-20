d2ea01dfeed89376eb7c65ef76a30c55230cfe22
Mashiroycz <yinchengzong@corp-ci.com>
2020-07-20 16:45:45 +0800

    v-0.0.1

---
+++ src/pages/test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>测试页面{{a}}</div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+      a: 'sd'
+    }
+  }
+}
+</script>
+
+<style>
+div{
+  color:aqua;
+}
+</style>
